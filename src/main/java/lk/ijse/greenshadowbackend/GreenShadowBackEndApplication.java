9bc2866ce69ddbd4e4619738730cadaf39f40142
amliyanage <ashenmliyanage@gmail.com>
2024-10-28 20:58:22 +0530

    project initialized

---
+++ src/main/java/lk/ijse/greenshadowbackend/GreenShadowBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowBackEndApplication.class, args);
+    }
+
+}
