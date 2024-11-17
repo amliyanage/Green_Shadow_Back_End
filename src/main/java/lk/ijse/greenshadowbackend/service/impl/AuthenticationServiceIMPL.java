19a8163b780e3c7020ffe3bf3ae536368353cff7
amliyanage <ashenmliyanage@gmail.com>
2024-11-17 17:53:24 +0530

    update sigUp end point

--- src/main/java/lk/ijse/greenshadowbackend/service/impl/AuthenticationServiceIMPL.java
+++ src/main/java/lk/ijse/greenshadowbackend/service/impl/AuthenticationServiceIMPL.java
@@ -2,6 +2,9 @@ package lk.ijse.greenshadowbackend.service.impl;
 
 import lk.ijse.greenshadowbackend.Repository.UserRepository;
 import lk.ijse.greenshadowbackend.dto.impl.UserDTO;
+import lk.ijse.greenshadowbackend.entity.User;
+import lk.ijse.greenshadowbackend.exception.AlreadyExistsException;
+import lk.ijse.greenshadowbackend.exception.DataPersistFailedException;
 import lk.ijse.greenshadowbackend.jwtmodels.JwtAuthResponse;
 import lk.ijse.greenshadowbackend.jwtmodels.SignIn;
 import lk.ijse.greenshadowbackend.service.AuthenticationService;
@@ -12,6 +15,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -35,9 +40,17 @@ public class AuthenticationServiceIMPL implements AuthenticationService {
 
     @Override
     public JwtAuthResponse signUp(UserDTO signUp) {
-        var savedUser = userService.save(mapping.convertUserDTOToUser(signUp));
-        var genToken = jwtService.generateToken(savedUser);
-        return JwtAuthResponse.builder().token(genToken).build();
+        Optional<User> existsUser = userService.findByEmail(signUp.getEmail());
+        if (!existsUser.isPresent()){
+            User savedUser = userService.save(mapping.convertUserDTOToUser(signUp));
+            if (savedUser == null){
+                throw new DataPersistFailedException("User Save Failed");
+            }
+            var genToken = jwtService.generateToken(savedUser);
+            return JwtAuthResponse.builder().token(genToken).build();
+        }else {
+            throw new AlreadyExistsException("User already exists");
+        }
     }
 
     @Override
